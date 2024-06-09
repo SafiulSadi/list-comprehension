a95b742e7e002909a9c6ed2e178830227876f98c
A. S. M. Safiul Sadi <41629377+SafiulSadi@users.noreply.github.com>
2024-06-09 10:30:14 +0600

    no contribution

--- main.py
+++ main.py
@@ -10,7 +10,18 @@ print( new_list)
 # new_list  =  [new_item for item in list if test]
 names = ["Alex", "Beth", "Caroline", "Dave", "Elenor", "Freddie"]
 short_names = ["hi, " + name for name in names if len(name)< 5]
-print(names)
-print(short_names)
+# print(names)
+# print(short_names)
 uppercase_names = [name.upper() for name in names if len(name)>=5]
-print(uppercase_names)
+# print(uppercase_names)
+
+# dictionary comprehension
+# new_dict = {new_key:new_value for item  in list}
+# new_dict = {new_key: new_value for (key, value) in dict.items()}
+# new_dict = {new_key: new_value for (key, value) in dict.items() if test}
+import random
+students_scores = {student: random.randint(1,100) for student in names}
+
+print(students_scores)
+# with dictionaries is similar but need to work on it more 
+
